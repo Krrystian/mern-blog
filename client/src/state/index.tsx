b4c198ef2cf5fde9da8579eaf75277ab82c7a0b8
Krystian Cichorz <krystiancichorz708@gmail.com>
2023-09-03 22:54:02 +0200

    Initial Commit

---
+++ client/src/state/index.tsx
@@ -0,0 +1,48 @@
+import { createSlice, isAction } from "@reduxjs/toolkit";
+
+interface CounterState {
+  user: { friends: any } | null;
+  token: string | null;
+  posts: any;
+}
+const initialState: CounterState = {
+  user: null,
+  token: null,
+  posts: [],
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setLogin: (state, action) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+    },
+    setLogout: (state) => {
+      state.user = null;
+      state.token = null;
+    },
+    setFriends: (state, action) => {
+      if (state.user) {
+        state.user.friends = action.payload.friends;
+      } else {
+        console.log("user friends non-existant");
+      }
+    },
+    setPosts: (state, action) => {
+      state.posts = action.payload.posts;
+    },
+    setPost: (state, action) => {
+      const updatedPosts = state.posts.map((post: any) => {
+        if (post._id === action.payload.post_id) return action.payload.post;
+        return post;
+      });
+      state.posts = updatedPosts;
+    },
+  },
+});
+
+export const { setFriends, setLogin, setLogout, setPost, setPosts } =
+  authSlice.actions;
+export default authSlice.reducer;
