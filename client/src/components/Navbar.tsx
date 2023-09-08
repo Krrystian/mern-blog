dc28f7c72788110f1885ce365979a72b854a95a5
Krystian Cichorz <krystiancichorz708@gmail.com>
2023-09-09 01:02:57 +0200

    -multer + Navbar styling

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,13 +1,16 @@
-import React, { useState } from "react";
-import { set } from "react-hook-form";
+import { useState } from "react";
+import { BiSearchAlt } from "react-icons/bi";
 
 const Navbar = () => {
   const [search, setSearch] = useState("");
-
+  const [searchResults, setSearchResults] = useState([]);
+  const handleSearch = () => {
+    console.log(search);
+  };
   return (
     <div className="h-[100px] w-screen z-10 text-[#DC6A00] border-b-2 border-[#DC6A00] text-4xl flex items-center text-center bg-transparent">
       <p className="w-[30%] cursor-default select-none">whY?</p>
-      <div className="w-[40%] h-full flex items-center">
+      <div className="w-[40%] h-full flex items-center relative">
         <input
           className=" text-white/80 p-3 w-full bg-white/10 rounded-xl placeholder-white/80 focus:outline-none placeholder:text-center"
           placeholder="Search"
@@ -20,6 +23,10 @@ const Navbar = () => {
             }
           }}
         ></input>
+        <BiSearchAlt
+          className="absolute right-2 cursor-pointer"
+          onClick={handleSearch}
+        />
       </div>
 
       <div className="w-[30%] text-3xl">New Post</div>
--- server/index.js
+++ server/index.js
@@ -3,11 +3,8 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import multer from "multer";
 import morgan from "morgan";
-import path from "path";
 import helmet from "helmet";
-import { fileURLToPath } from "url";
 import { register } from "./controllers/auth.js";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
@@ -21,8 +18,6 @@ import Post from "./models/Post.js";
 import { users, posts } from "./data/index.js";
 
 // CONFIGURATION
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -32,27 +27,15 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: "true" }));
 app.use(cors());
-app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
-// FILE STORAGE
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "public/assets");
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
-});
-const upload = multer({ storage });
-
-// ROUTES with files
+// POST REQUESTS
 app.post("/auth/register", register);
-app.post("/posts", verifyToken, upload.single("picture"), createPost);
+app.post("/posts", verifyToken, createPost);
 
-//ROUTES
+// ROUTES
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
-app.use("/posts", postRoutes); //1h13min34s
+app.use("/posts", postRoutes);
 // DATABASE
 mongoose
   .connect(process.env.MONGO_URL, {
