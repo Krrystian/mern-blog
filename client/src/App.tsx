4c7645caa23e7573097983e7f6e53b0ea2eea4db
Krystian Cichorz <krystiancichorz708@gmail.com>
2023-10-03 01:30:30 +0200

    Adding vercel.json

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -25,11 +25,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<LoginPage />} />
-          <Route
-            path="/home"
-            element={<HomePage />}
-            errorElement={<>ERROR</>}
-          />
+          <Route path="/home" element={<HomePage />} />
           <Route path="/profile/:userId" element={<ProfilePage />} />
         </Routes>
       </BrowserRouter>
